724afc4028264c59dd7ebef1afc18c7eb15eb659
Philip <philip.korthauer@gmx.at>
2023-01-12 22:08:23 +0100

    t

--- PHP/loadproduct.php
+++ PHP/loadproduct.php
@@ -100,6 +100,7 @@ for ($i=0; $i < count($posts) / 9; $i++) {
     echo "<li class='page-item'><a class='page-link active mt-3' href='#' onclick='loadproduct($i)'>".$count."</a></li>";
   }else{
     echo "<li class='page-item'><a class='page-link mt-3' href='#' onclick='loadproduct($i)'>".$count."</a></li>";
+    
   }
 }
 echo "</ul>
